a6343e07d288a76ccf7737ebb1870a6882dcbb8b
Ollie Lewington <oliver.lewington@atos.net>
2026-03-25 19:11:04 +0000

    Setting up UI and Navbar for account pages.

---
+++ FellsideDigital/Components/Layout/Navbar.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace FellsideDigital.Components.Layout;
+
+public partial class Navbar : ComponentBase, IDisposable
+{
+    private bool _mobileMenuOpen;
+    private bool _hasNotifications = true;
+
+    private string? currentUrl;
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+
+    private static readonly (string Label, string Href)[] _navLinks =
+    {
+        ("Home", "/"),
+        ("Contact", "/contact")
+    };
+
+    protected override void OnInitialized()
+    {
+        currentUrl = NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
+        NavigationManager.LocationChanged += OnLocationChanged;
+    }
+
+    private void OnLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        currentUrl = NavigationManager.ToBaseRelativePath(e.Location);
+        StateHasChanged();
+    }
+
+    private void ToggleMobileMenu() => _mobileMenuOpen = !_mobileMenuOpen;
+    private void CloseMobileMenu() => _mobileMenuOpen = false;
+
+    public void Dispose()
+    {
+        NavigationManager.LocationChanged -= OnLocationChanged;
+    }
+}
